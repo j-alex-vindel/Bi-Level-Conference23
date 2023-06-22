e254af558579ef6e3296777e2d45e92fd3462caa
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-06-22 13:27:41 +0100

    side bar and links

--- Welcome.py
+++ Welcome.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
 st.title('Welcome')
+
+st.sidebar.success("Select a page to visit")
+
+st.markdown("""    
+
+            ### Want to know more?
+            - View the [poster](https://j-alex-vindel-bi-level-conference23.streamlit.app/Poster)
+            - Check out a quick explanation video [here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Video)
+            #### Want to know more aboute me?
+            - Click [here](https://j-alex-vindel-bi-level-conference23.streamlit.app/About_Me) for a quick summary and social media links
+
+
+
+
+
+
+            """)
