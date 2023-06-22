aac05048645fba7259f915cf503b5e6692e2af6e
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-06-22 13:43:39 +0100

    welcome wording

--- Welcome.py
+++ Welcome.py
@@ -7,12 +7,12 @@ st.sidebar.success("Select a page to visit")
 st.markdown("""    
 
             ### Information About Poster
-            - View the Poster by simply clicking the page Poster or [click here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Poster)
-            - Check out a quick explanation video at the Video page or [here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Video)
+            - View the poster by simply clicking the page 'Poster' or [click here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Poster)
+            - Check out a quick explanation video at the 'Video' page or [here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Video)
             
             
             #### Want to know more aboute me?
-            - Click [here](https://j-alex-vindel-bi-level-conference23.streamlit.app/About_Me) for profile and social media links
+            - Profile and social media links [Click Me](https://j-alex-vindel-bi-level-conference23.streamlit.app/About_Me)
 
 
             """)
