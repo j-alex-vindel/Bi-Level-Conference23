540dae0703ed73bb6d35b8d3d2103bfd79047dad
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-06-22 13:14:17 +0100

    Pages to pages

--- pages/1_Poster.py
+++ pages/1_Poster.py
@@ -4,6 +4,6 @@ from PIL import Image
 
 st.write('Poster')
 
-image = Image.open('Pages/Pics/poster_dsms2023.jpg')
+image = Image.open('pages/Pics/poster_dsms2023.jpg')
 
 st.image(image,caption='Poster')
