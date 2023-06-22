6d997a1ba9ca395ede5df2da15b46fc197c3bd2e
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-06-22 13:51:47 +0100

    welcome message

--- Welcome.py
+++ Welcome.py
@@ -5,7 +5,9 @@ st.title('Welcome to More Information')
 st.sidebar.success("Select a page to visit")
 
 st.markdown("""    
-
+            ## I am pleased you've decided to scan my QR code. In this page you will find extra information about the topic and poster.
+            Please use the pages on left of the screen to navigate or follow the links below. I'd be thrilled to have comments and/or feddbacks.
+            
             ### Information About Poster
             - View the poster by simply clicking the page 'Poster' or [click here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Poster)
             - Check out a quick explanation video at the 'Video' page or [here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Video)
