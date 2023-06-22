7150bae44fa7e5b6e1912be88ee4527f809e6369
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-06-22 14:27:45 +0100

    include name

--- Welcome.py
+++ Welcome.py
@@ -6,17 +6,12 @@ st.sidebar.success("Select a page to visit")
 
 st.markdown("""    
             ## I am pleased you've decided to scan my QR code. In this page you will find extra information about the topic and poster.
-            Please use the pages on left of the screen to navigate or follow the links below. I'd be thrilled to have comments and/or feddbacks.
+            Please use the pages on left of the screen to navigate or follow the links below. I'd be thrilled if you leave comments and/or feddbacks.
 
             ### Information About Poster
             - View the poster by simply clicking the page 'Poster' or [click here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Poster)
             - Check out a quick explanation video at the 'Video' page or [here](https://j-alex-vindel-bi-level-conference23.streamlit.app/Video)
             
-            
-            #### Want to know more aboute me?
-            - Profile and social media links [Click Me](https://j-alex-vindel-bi-level-conference23.streamlit.app/About_Me)
-
-
             """)
 
 st.divider()
@@ -24,7 +19,17 @@ st.markdown("""
 #### Leave a Comment
   """)
 input_name = st.text_input('Name')
+input_mail = st.text_input('Mail (optional)')
 input_text = st.text_area('Enter text')
 st.caption('Comments will be saved and checked')
+
 if st.button('Submit'):
     st.write(input_text)
+
+st.divider()
+
+st.markdown("""
+     #### Want to know more aboute me?
+            - Profile and social media links [Click Me](https://j-alex-vindel-bi-level-conference23.streamlit.app/About_Me)
+
+""")
