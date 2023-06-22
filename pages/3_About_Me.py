7fadfda034e8e13b7c131f03eab8a5d543e5fedc
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-06-22 13:18:05 +0100

    Social media added

--- pages/3_About_Me.py
+++ pages/3_About_Me.py
@@ -1,4 +1,29 @@
 import streamlit as st
 from PIL import Image
 
-st.write('Socials')
+
+st.header('About Me')
+
+st.divider()
+
+st.header('Social Media')
+
+col1,col2 = st.columns(2,gap='medium')
+
+with col1:
+    
+    linkedin = Image.open('pages/Pics/linkedin1.PNG')
+
+    st.image(linkedin,caption='LinkedIn',output_format='PNG')
+    st.markdown('## [**alexander-vindel**](https://www.linkedin.com/in/alexander-vindel/)')
+
+with col2:
+    
+    github = Image.open('pages/Pics/github1.PNG')
+
+    st.image(github,caption='Github',output_format='PNG')
+    st.markdown('## [**j-alex-vindel**](https://github.com/j-alex-vindel)')
+
+with st.expander('''Icon's Credits'''):
+    st.caption('''https://www.flaticon.com/free-icons/linkedin''')
+    st.caption('''https://www.flaticon.com/free-icons/github''')
