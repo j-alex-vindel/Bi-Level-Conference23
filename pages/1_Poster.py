b2afd0222c57500d2f9e9410e8ea9a454152aae5
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-07-17 14:54:30 +0100

    poster now updated

--- pages/1_Poster.py
+++ pages/1_Poster.py
@@ -6,7 +6,7 @@ st.markdown("""
             ## :purple[Poster] 
             """)
 
-image = Image.open('pages/Pics/poster_ICBO23.jpg')
+image = Image.open('pages/Pics/poster_ICBO23(1).jpg')
 
 st.image(image,caption='Poster')
 
